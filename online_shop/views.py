1c22940d1cca1a8d52137aa7c370d7b7b48f59b4
wezymord <hubert.hernoga@wp.pl>
2018-03-20 16:22:39 +0100

    Added new order to db

--- online_shop/views.py
+++ online_shop/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.views import View
 from django.shortcuts import render
 from django.shortcuts import redirect
-from .models import Product, ShippingOption
+from .models import Product, ShippingOption, Order, User
 from django.http import QueryDict
 
 
@@ -134,7 +134,7 @@ class CheckoutAddress(View):
             company = request.POST.get('company')
             country = request.POST.get('country')
             city = request.POST.get('city')
-            zip = request.POST.get('postal_code')
+            postal_code = request.POST.get('postal_code')
             address1 = request.POST.get('address1')
             address2 = request.POST.get('address2')
 
@@ -145,7 +145,7 @@ class CheckoutAddress(View):
             order['company'] = company
             order['country'] = country
             order['city'] = city
-            order['postal_code'] = zip
+            order['postal_code'] = postal_code
             order['address1'] = address1
             order['address2'] = address2
 
@@ -193,10 +193,26 @@ class CheckoutReview(View):
         else:
             return render(request, 'checkout-review.html')
 
-    def post(self, request):
-        return render(request, 'checkout-review.html')
-
 
 class CheckoutComplete(View):
     def get(self, request):
+        product_ids = request.session['basket'].keys()
+        order = request.session['order']
+        shipping_option = ShippingOption.objects.get(pk=request.session['shipping_method']['shipping_method_id'])
+
+        user = User(first_name=order['first_name'], last_name=order['last_name'], e_mail=order['email'],
+                    phone_number=order['phone_number'], company=order['company'], country=order['country'],
+                    city=order['city'], postal_code=order['postal_code'], address1=order['address1'],
+                    address2=order['address2'])
+        user.save()
+
+        make_order = Order(user=user)
+        make_order.save()
+
+        for id in product_ids:
+            for product in Product.objects.filter(pk=id):
+                make_order.products.add(product)
+
+        make_order.shipping_options.add(shipping_option)
+
         return render(request, 'checkout-complete.html')
