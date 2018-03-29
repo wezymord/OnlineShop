dbc69ad1d6873ebc17dd046c18e20b61a68a15cf
wezymord <hubert.hernoga@wp.pl>
2018-03-29 16:07:17 +0200

    Validated email and set to be unique

--- online_shop/forms.py
+++ online_shop/forms.py
@@ -1,5 +1,17 @@
 from django import forms
-from django.forms import ModelForm
+from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
 
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['email']
+        widgets = {
+            'email': forms.TextInput(attrs={'class': 'form-control', 'type': 'email', 'id': 'checkout-email'})
+        }
 
-
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if email and User.objects.filter(email=email).exists():
+            raise ValidationError("E-mail: {} already exist!".format(email))
+        return email
