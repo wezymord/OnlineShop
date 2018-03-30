d8ab260c268f0ba456777d8ef994b74b4d5e1c7f
wezymord <hubert.hernoga@wp.pl>
2018-03-30 23:00:47 +0200

    Refactoring postal code validation

--- online_shop/validators/postal_code.py
+++ online_shop/validators/postal_code.py
@@ -1,9 +1,8 @@
-from django.core.exceptions import ValidationError
 import re
 
 
-class PostalCodeValidator():
-    zip_to_regex = {
+class PostalCodeValidator:
+    country_code_to_regex = {
     "GB": "GIR[ ]?0AA|((AB|AL|B|BA|BB|BD|BH|BL|BN|BR|BS|BT|CA|CB|CF|CH|CM|CO|CR|CT|CV|CW|DA|DD|DE|DG|DH|DL|DN|DT|DY|E|EC|EH|EN|EX|FK|FY|G|GL|GY|GU|HA|HD|HG|HP|HR|HS|HU|HX|IG|IM|IP|IV|JE|KA|KT|KW|KY|L|LA|LD|LE|LL|LN|LS|LU|M|ME|MK|ML|N|NE|NG|NN|NP|NR|NW|OL|OX|PA|PE|PH|PL|PO|PR|RG|RH|RM|S|SA|SE|SG|SK|SL|SM|SN|SO|SP|SR|SS|ST|SW|SY|TA|TD|TF|TN|TQ|TR|TS|TW|UB|W|WA|WC|WD|WF|WN|WR|WS|WV|YO|ZE)(\d[\dA-Z]?[ ]?\d[ABD-HJLN-UW-Z]{2}))|BFPO[ ]?\d{1:4}",
     "JE": "JE\d[\dA-Z]?[ ]?\d[ABD-HJLN-UW-Z]{2}",
     "GG": "GY\d[\dA-Z]?[ ]?\d[ABD-HJLN-UW-Z]{2}",
@@ -165,9 +164,5 @@ class PostalCodeValidator():
     }
 
     def is_valid(self, country_code, postal_code):
-        for zip, regex in self.zip_to_regex.items():
-            if zip == country_code:
-                if re.match(self.zip_to_regex[zip], postal_code):
-                    return True
-                else:
-                    return False
+        regex = self.country_code_to_regex[country_code]
+        return re.match(regex, postal_code)
