013005cc0aec9f77fdcbff08914b97f5893d03f0
wezymord <hubert.hernoga@wp.pl>
2018-04-02 19:17:10 +0200

    CSRF token passed as POST data with each POST request

--- static/basket_details/basket.js
+++ static/basket_details/basket.js
@@ -1,21 +1,34 @@
 $(document).ready(function() {
 
-    function getCookie(c_name) {
-
-        if (document.cookie.length > 0) {
-            c_start = document.cookie.indexOf(c_name + "=");
-            if (c_start != -1) {
-                c_start = c_start + c_name.length + 1;
-                c_end = document.cookie.indexOf(";", c_start);
-                if (c_end == -1) c_end = document.cookie.length;
-                return unescape(document.cookie.substring(c_start, c_end));
+    function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+                }
             }
         }
-        return "";
+        return cookieValue;
+    }
+    var csrftoken = getCookie('csrftoken');
+
+
+      function csrfSafeMethod(method) {
+        // these HTTP methods do not require CSRF protection
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
     }
 
+
     $.ajaxSetup({
-        headers: {"X-CSRFToken": getCookie("csrftoken")}
+        beforeSend: function(xhr, settings) {
+            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
+        }
     });
 
 
