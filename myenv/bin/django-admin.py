382bb441f0938ae8b0eba2bee8dad14f094d8a05
wezymord <hubert.hernoga@wp.pl>
2018-04-13 01:40:41 +0200

    Commented pops

--- myenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/hubert/Projects/pawel_marly-online_shop/OnlineShop/myenv/bin/python3.6
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
