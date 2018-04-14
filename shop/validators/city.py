a7da5918ecfef377206b7b48034f642604e91538
wezymord <hubert.hernoga@wp.pl>
2018-04-14 23:33:37 +0200

    Displaying validation after pressing the button

--- shop/validators/city.py
+++
@@ -1,7 +0,0 @@
-from django.core.exceptions import ValidationError
-
-
-def clean_city(city):
-    if not city.isalpha():
-        raise ValidationError("Only alphanumeric characters are allowed.")
-    return city
