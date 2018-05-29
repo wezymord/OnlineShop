ebae21126df74b0be2437971d2d8aed6f0b699ec
wezymord <hubert.hernoga@wp.pl>
2018-05-29 21:17:05 +0200

    Changes regarding README

--- staticfiles/admin/js/vendor/select2/select2.full.js
+++ staticfiles/admin/js/vendor/select2/select2.full.js
@@ -438,7 +438,7 @@ var requirejs, require, define;
 
     define = function (name, deps, callback) {
         if (typeof name !== 'string') {
-            throw new Error('See almond README: incorrect module build, no module name');
+            throw new Error('See almond README.md: incorrect module build, no module name');
         }
 
         //This module may not have dependencies
