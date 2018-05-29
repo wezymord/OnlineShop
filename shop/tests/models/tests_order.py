30040e673f1784d8e6a933dcd5c026d66f2ea4db
wezymord <hubert.hernoga@wp.pl>
2018-05-30 00:56:43 +0200

    Refactoring testing order

--- shop/tests/models/tests_order.py
+++ shop/tests/models/tests_order.py
@@ -13,65 +13,51 @@ class TestOrders(TestCase):
 
     def test_order_total_price_when_quantity_is_0(self):
         order = Order.objects.create(user=self.user, shipping_option=self.shipping_1)
-        order.save()
         order.products.add(self.product_1)
-        sale = Sale.objects.create(product=self.product_1, order=order)
-        sale.save()
+        Sale.objects.create(product=self.product_1, order=order)
         order.save()
         self.assertEqual(order.total_price, 19.00)
 
     def test_order_total_price_when_quantity_is_3(self):
         order = Order.objects.create(user=self.user, shipping_option=self.shipping_2)
-        order.save()
         order.products.add(self.product_2)
-        sale = Sale.objects.create(product=self.product_2, order=order, quantity=3)
-        sale.save()
+        Sale.objects.create(product=self.product_2, order=order, quantity=3)
         order.save()
         self.assertEqual(order.total_price, 13.00)
 
     def test_order_total_price_when_quantity_is_43(self):
         order = Order.objects.create(user=self.user, shipping_option=self.shipping_1)
-        order.save()
         order.products.add(self.product_1)
-        sale = Sale.objects.create(product=self.product_1, order=order, quantity=43)
-        sale.save()
+        Sale.objects.create(product=self.product_1, order=order, quantity=43)
         order.save()
         self.assertEqual(order.total_price, 523.00)
 
     def test_order_date(self):
         order = Order.objects.create(user=self.user, shipping_option=self.shipping_1)
-        order.save()
         order.products.add(self.product_1)
-        sale = Sale.objects.create(product=self.product_1, order=order, quantity=43)
-        sale.save()
+        Sale.objects.create(product=self.product_1, order=order, quantity=43)
         order.save()
         self.assertEqual(order.date, datetime.date.today())
 
     def test_order_sale(self):
         order = Order.objects.create(user=self.user, shipping_option=self.shipping_1)
-        order.save()
         order.products.add(self.product_1)
         sale = Sale.objects.create(product=self.product_1, order=order, quantity=43)
-        sale.save()
         order.save()
         for product in order.products.all():
             self.assertEqual(product, sale.product)
 
     def test_sale_price_for_product_1(self):
         order = Order.objects.create(user=self.user, shipping_option=self.shipping_1)
-        order.save()
         order.products.add(self.product_1)
         sale = Sale.objects.create(product=self.product_1, order=order, quantity=43)
-        sale.save()
         order.save()
         self.assertEqual(sale.price, self.product_1.price)
 
     def test_sale_price_for_product_2(self):
         order = Order.objects.create(user=self.user, shipping_option=self.shipping_1)
-        order.save()
         order.products.add(self.product_2)
         sale = Sale.objects.create(product=self.product_2, order=order, quantity=43)
-        sale.save()
         order.save()
         self.assertEqual(sale.price, self.product_2.price)
 
